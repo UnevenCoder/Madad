c01837cb5748fddf6491b98ad779bbf05788a314
Ameen <ameenshafeeq2005@gmail.com>
2020-08-15 16:39:30 +0000

    joke fixed

--- joke.js
+++ joke.js
@@ -6,7 +6,12 @@ exports.joke =(ab)=>{fetch('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags
 .then(res=>res.json())
 .then(data=>{
   console.log(data)
+  if(data.delivery){
   ab.send({embed:embed.embedded('',data.delivery,data.setup,'','','Joke by madad :)')})
-  
+    
+  }
+  else{
+    
+   ab.send({embed:embed.embedded('',data.joke,'','','','Joke by madad :)')})
   })
 }
