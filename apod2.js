b6f94338830290380bd01cbc424f5615608b41b8
Ameen <ameenshafeeq2005@gmail.com>
2020-08-16 06:10:51 +0000

    Nasapod

--- apod2.js
+++ apod2.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch')
 const Discord = require('discord.js');
 const embed = require('./embed.js')
-let key = process.env.nasa
+//let key = process.env.nasa
 exports.apod2=(ab,date)=>{
-  fetch('https://api.nasa.gov/planetary/apod?api_key=+key++date.toString())
+  fetch('https://api.nasa.gov/planetary/apod?api_key='+key+'date='+date.toString())
 .then(res=>res.json())
 .then(data=>{
   
