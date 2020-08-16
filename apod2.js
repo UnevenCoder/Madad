bc3d12a3ec1e34d87a31ef4f9eb1d1a3eee20734
Ameen <ameenshafeeq2005@gmail.com>
2020-08-16 06:14:30 +0000

    apod 2 date feature

--- apod2.js
+++ apod2.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const embed = require('./embed.js')
 //let key = process.env.nasa
 exports.apod2=(ab,date)=>{
-  fetch('https://api.nasa.gov/planetary/apod?api_key='+key+'date='+date.toString())
+  fetch('https://api.nasa.gov/planetary/apod?api_key='+key+'&date='+date.toString())
 .then(res=>res.json())
 .then(data=>{
   
