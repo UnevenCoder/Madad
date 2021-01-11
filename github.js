49a75d75ae633381e1bb703c11996f8187bf09b3
Ameen Shafeeq <ameenshafeeq2005@gmail.com>
2021-01-11 10:54:25 +0000

    Github command edited

--- github.js
+++ github.js
@@ -6,7 +6,7 @@ exports.github=(ab,name)=>{
 fetch('https://api.github.com/users/'+name)
 .then(res=>res.json())
 .then(data=>{
-  if(data.followers){
+  if(data.type!='Organization' && data.followers){
   let msg = new Discord.MessageEmbed()
 	.setColor('#0099ff')
 //	.setTitle()//+data.definitions[0].emoji)
@@ -21,7 +21,31 @@ fetch('https://api.github.com/users/'+name)
 	
 	ab.send({embed:msg})}
 	else{
-	  ab.send('Username not found try again please :)')
-	}
+	  fetch('https://api.github.com/orgs/'+name)
+.then(res=>res.json())
+.then(data=>{
+//  console.log(data)
+  if(data.name){
+  let msg = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+//	.setTitle()//+data.definitions[0].emoji)
+	.setAuthor(data.company?data.name+' , '+data.company:data.name)
+	.addFields(
+		{ name: 'Blog', value: data.blog? data.blog :'User has no blog', inline: true },
+		{ name: 'Location', value: data.location, inline: true },
+			{ name: 'Twitter', value: data.twitter_username, inline: true },
+			{ name:'Email', value:data.email,inline:true},
+			{name:'Verified' ,value:data.is_verified?'Yes':'No',inline:true}
+	)
+	.setImage(data.avatar_url)
+	.setTimestamp()
+	
+	ab.send({embed:msg})
+  }
+    else{
+      ab.send('Username / Organization not found sry :(')
+    }
+  
+	
 })
-}
+}})}
